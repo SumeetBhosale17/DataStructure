0ad082684bad93a54253eedee2c6f3eda17fc313
SumeetBhosale17 <sumeetbhosale1947@gmail.com>
2024-11-15 11:50:06 +0530

    some changes

--- Array/maxElementArray.c
+++ Array/maxElementArray.c
@@ -1,4 +1,5 @@
 //Given an array of integers arr that is first strictly increasing and then maybe strictly decreasing, find the maximum element in the array. 
+
 /*
 Name : Sumeet Sambhaji Bhosale
 Roll No : 2021
@@ -6,7 +7,6 @@ URN : 23031024
 Class : SY - A
 Batch : S2
 */
-
 
 #include <stdio.h>
 
@@ -61,16 +61,19 @@ int maxElement(int *arr, int low, int high)
 /*
 Complexity:
     -> Time Complexity: O(logn)
-        Best Case: O(1) --- Constant -> {When the array has only one element or the when the array is strictly increasing or decreasing}
+        Best Case: O(1) --- Constant -> {When the array has only one element or the when the array is strictly increasing or decreasing or the maximum element is at middle}
         Worst Case: O(logn) --- Logarithmic -> {General Case}
     -> Space Complexity: O(logn)
         Best Case : O(1) --- Constant -> {When the array has only one element or the when the array is strictly increasing or decreasing}
         Worst Case : O(logn) --- Logarithmic -> {General Case}
 
 Description:
-    -> If the array has only one element, return that element as it is the maximum.
-        When the array is strictly increasing or decreasing, the maximum element is at the beginning or end of the array.
-        The function returns respective values -> Thus giving a constant complexity
+    ->  - If the array has only one element, return that element as it is the maximum.
+        - When the array is strictly increasing or decreasing, the maximum element is at the beginning or end of the array.
+            The function returns respective values -> Thus giving a constant complexity
+        - If the maximum element is present in the middle of the array, It returns the element without any recursive calls. -> Constant complexity
+        - If all elements are equal, then also the complexity is constant as it satisfies the condition of middle element as maximum
+
     -> For general case, If the middle element is greater than the elements on both sides, then it is the maximum element.
         Otherwise the array is divided into two halves and the maximum element is found recursively in the half where the maximum element is present.
         If the middle element is less than the element in the left, then the greatest element is in the left side
@@ -83,17 +86,27 @@ Description:
 // Main Function
 int main()
 {
-    printf("Enter Size of the array : ");
+    printf("Enter Size of the array (3 - 105) : ");
     int size;
     scanf("%d", &size);
-    if (size <= 0)  // checks if the size is valid or invalid
+    if (size < 3 || size > 105)  // checks if the size is valid or invalid
     {
-        printf("Invalid array size!\n");
-        return -1;
+        printf("Invalid array size!\nPlease Re-Enter\n");
+        main();
     }
-    int arr[size];
-    arrInput(arr, size);
-    arrPrint(arr, size);
-    printf("\nThe maximum element in the array is : %d\n", maxElement(arr, 0, size - 1));
+    /*
+    We have been given the constaints that size can be from 3 to 105,
+    So anything other than that will be invalid
+    And the user may enter invalid size
+    This Condition checks if it is valid or not
+    If it is valid the programs execute as it is
+    If it is invalid, The user is asked to re-enter the value
+    By calling main() recursively
+    Thus Ensuring valid size
+    */
+    int arr[size]; // declaring integer array of size entered by the user
+    arrInput(arr, size); // taking input of array 
+    arrPrint(arr, size); // printing the array
+    printf("\nThe maximum element in the array is : %d\n", maxElement(arr, 0, size - 1)); // Finding the maximum element the array
     return 0;
 }
--- Array/transitionPoint.c
+++ Array/transitionPoint.c
@@ -1,4 +1,5 @@
 //Find Transition point in a sorted array containing only 0s and 1s
+
 /*
 Name : Sumeet Sambhaji Bhosale
 Roll No : 2021
@@ -6,8 +7,8 @@ URN : 23031024
 Class : SY - A
 Batch : S2
 */
+
 #include <stdio.h>
-#include <stdlib.h>
 
 // Function to take input of array of integers
 void arrInput(int* arr, int size)
@@ -15,22 +16,35 @@ void arrInput(int* arr, int size)
     printf("Enter elements in the array (0 and 1 only)\n");
     for (int i = 0; i<size; i++)
     {
-        printf("Enter element %d : ", i+1);
-        scanf("%d", &arr[i]);
-        // Checking input
-        if (arr[i] != 0 && arr[i] != 1) 
+        int valid = 0;
+        // If the user enters invalid input it prompts a message stating it the input is invalid and asks to re-enter
+        while(!valid)
         {
-            printf("Invalid Element!\nOnly 1 and 0 is allowed!");
-            exit(1); // Exit if invalid input
-        }
-        // Checking if the array is sorted
-        if (i > 0 && arr[i-1] > arr[i])
-        {
-            printf("Invalid Input! \nThe array must be sorted");
-            exit(1);
+            printf("Enter element %d : ", i+1);
+            scanf("%d", &arr[i]);
+            // Checking if the input is valid (0 or 1)
+            if (arr[i] != 0 && arr[i] != 1)
+            {
+                printf("Invalid Input!\nOnly 0 and 1 is allowed!\nPlease Re-enter the value\n");
+                continue;
+            }
+            // Checking if the array is sorted or not
+            if (i > 0 && arr[i-1] > arr[i])
+            {
+                printf("Invalid Input!\nThe Array should be sorted!\nPlease Re-Enter the value\n");
+                continue;
+            }
+            valid = 1; // Input will be valid, Thus breaks the while loop and continuing the same process for next index
         }
     }
 }
+/*
+The User may input which may be invalid in this case. 
+Rather than working on a wrong input, Checking the conditions before hand would be a safer approach, 
+The above function which takes input into the array checks those conditions.
+If the user inputs enters a invalid input, this functions informs the user that the enterred input is invalid
+and User gets the opportunity to re-enter the input.
+*/
 
 // Function to print the elements in the array
 void arrPrint(int* arr, int size)
@@ -42,15 +56,15 @@ void arrPrint(int* arr, int size)
     }
 }
 
-// Function to find the transition points in the array
+// Function to find the transition points in the array (The point where 1 is first observed)
 int transitionPoint(int* arr, int low, int high)
 {
     if (low > high) return -1; // invalid low and high
     if (arr[high] == 0) return -1; // if the array contains only 0s
     if (arr[low] == 1) return low;  // if the array contains only 1s
 
-    int mid = low + (high - low) / 2;
-    if (arr[mid] == 1 && (arr[mid-1] == 0 || mid == 0)) return mid;
+    int mid = low + (high - low) / 2; // Middle index of the array
+    if (arr[mid] == 1 && (arr[mid-1] == 0 || mid == 0)) return mid; // Returns middle index
     if (arr[mid] == 1) return transitionPoint(arr, low, mid-1);
     return transitionPoint(arr, mid+1, high);
 }
@@ -77,17 +91,27 @@ Description:
 //Main Function
 int main()
 {
-    printf("Enter size of the array : ");
+    printf("Enter size of the array (1 - 105) : ");
     int size;
     scanf("%d", &size);
-    if (size <= 0)  // checks if the size is valid or invalid
+    if (size <= 0 || size > 105)  // Checks if the size is valid or invalid
     {
-        printf("Invalid array size!\n");
-        return -1;
+        printf("Invalid array size!\nPlease Re-Enter\n");
+        main();
     }
-    int arr[size];
-    arrInput(arr, size);
-    arrPrint(arr, size);
-    printf("\nThe Transition Point in the array is : %d\n", transitionPoint(arr, 0, size-1));
+    /*
+    We have been given the constaints that size can be from 1 to 105,
+    So anything other than that will be invalid
+    And the user may enter invalid size
+    This Condition checks if it is valid or not
+    If it is valid the programs execute as it is
+    If it is invalid, The user is asked to re-enter the value
+    By calling main() recursively
+    Thus Ensuring valid size
+    */
+    int arr[size]; // declaration of integer array of size given by the user
+    arrInput(arr, size); // taking input for the array
+    arrPrint(arr, size); // printing the array
+    printf("\nThe Transition Point in the array is : %d\n", transitionPoint(arr, 0, size-1)); // prints the transition point
     return 0;
-}
+}    
